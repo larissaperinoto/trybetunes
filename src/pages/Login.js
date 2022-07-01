28b44f04ad127dece3ffeee1eff3ada9ac7f791d
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-01 18:22:00 -0300

    [Project Trybetunes] - Requisito 2.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import * as userAPI from '../services/userAPI';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      habilityButton: true,
+      isLoading: false,
+      redirect: false,
+      user: {
+        name: '',
+      },
+    };
+  }
+
+  handleButtonLogin = ({ target: { value } }) => {
+    const valueMin = 2;
+    if (value.length > valueMin) {
+      this.setState({ habilityButton: false, user: { name: value } });
+    } else {
+      this.setState({ habilityButton: true });
+    }
+  }
+
+  handleSubmitLogin = async () => {
+    const { createUser } = userAPI;
+    const { user } = this.state;
+    this.setState({ isLoading: true });
+    await createUser(user);
+    this.setState({ isLoading: false, redirect: true });
+  };
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { habilityButton, isLoading, redirect } = this.state;
+    return (
+      <div data-testid="page-login">
+        {isLoading ? <Loading />
+          : (
+            <div>
+              <label htmlFor="login-name-input">
+                <input
+                  type="text"
+                  name="login-name-input"
+                  data-testid="login-name-input"
+                  onChange={ this.handleButtonLogin }
+                />
+              </label>
+              <button
+                type="button"
+                disabled={ habilityButton }
+                data-testid="login-submit-button"
+                onClick={ this.handleSubmitLogin }
+              >
+                Entrar
+              </button>
+            </div>)}
+        { redirect && <Redirect to="/search" /> }
+      </div>
+    );
   }
 }
 
