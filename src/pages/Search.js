09ef6b1b1deca10c7cb213d48daeb9064924c9db
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-01 19:50:06 -0300

    [Project Trybetunes] - Requisito 5.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      habilityButton: true,
+    };
+  }
+
+  handleButtonSearch = ({ target: { value } }) => {
+    const valueMin = 1;
+    if (value.length > valueMin) {
+      this.setState({ habilityButton: false });
+    } else {
+      this.setState({ habilityButton: true });
+    }
+  }
+
   render() {
+    const { habilityButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="search-artist-input">
+          <input
+            type="text"
+            name="search-artist-input"
+            data-testid="search-artist-input"
+            onChange={ this.handleButtonSearch }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ habilityButton }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
