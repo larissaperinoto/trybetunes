939a119edafbdc6154ddda5d241533baef267be0
Larissa Perinoto <perinotolarissa@gmail.com>
2022-07-06 13:13:28 -0300

    [Project Trybetunes] - Requisito 13.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
-class Profile extends React.Component {
+class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userData: {},
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.importUserData();
+  }
+
+  importUserData = async () => {
+    const userData = await getUser();
+    this.setState({ isLoading: false, userData });
+  }
+
   render() {
+    const { isLoading, userData: { name, description, image, email } } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { isLoading ? <Loading /> : (
+          <div>
+            <Link to="/profile/edit">Editar perfil</Link>
+            <h3>{ name }</h3>
+            <img src={ image } alt={ `User ${name}` } data-testid="profile-image" />
+            <p>{ email }</p>
+            <p>{ description }</p>
+          </div>) }
       </div>
     );
   }
